da675e6e1b91387b7537776a40354fad36189837
RustyPy2 <chrisshirley92@gmail.com>
2023-10-23 16:24:52 -0400

    Update 1_Dashboard.py
    File did not upload correctly, insert correct code manually

--- 1_Dashboard.py
+++ 1_Dashboard.py
@@ -1,3 +1,35 @@
-import Streamlit as st
+import streamlit as st
+import pandas as pd
 
-st.header('Expense Report')
+st.set_page_config('Expense Report')
+
+hide_menu_style = """<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+st.header('Expense Report', anchor='dashboard')
+
+def file_Uploaded():
+
+    col1, col2, col3 = st.columns(3, gap='small')
+
+    df = pd.read_excel(io=excel_File, engine='openpyxl', usecols='A,B,C')
+
+    max_Val = df['Cost'].max()
+    min_Val = df['Cost'].min()
+
+    st.slider('Money Range', value=[min_Val, max_Val])
+    col1.selectbox('Categories', options= df['Category'].unique())
+    col2.selectbox('Vendors', options= df['Vendor'].unique())
+
+    category_Fig = df.drop(['Vendor'], axis=1)
+
+    fig = st.bar_chart(category_Fig, x='Category', y='Cost')
+
+
+excel_File = st.sidebar.file_uploader('Excel File', type='.xlsx')
+
+if excel_File is not None:
+    file_Uploaded()
